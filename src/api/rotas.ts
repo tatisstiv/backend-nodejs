a1f541f23ebe1c6218dd42690969ec04c2be9d50
tatiane <tati.s.stiv@gmail.com>
2020-06-27 21:34:24 -0300

    Adicionando guia para API

--- src/api/rotas.ts
+++ src/api/rotas.ts
@@ -2,5 +2,8 @@ import * as controlador from './operacoes';
 import {Router} from 'express';
 
 export const router = Router();
+router.get('/', (req, res) => {
+    res.send('Para realizar saque: http://localhost:5000/saque/(codigo da conta)/(valor do saque)<br/>Para realizar deposito: http://localhost:5000/deposito/(codigo da conta)/(valor do deposito)')
+})
 router.get(`/saque/:codigo/:valorSaque`, controlador.saque);
 router.get(`/deposito/:codigo/:valorDeposito`, controlador.deposito);
